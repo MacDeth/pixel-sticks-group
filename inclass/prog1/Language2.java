6f91615b313525fa55c9c1ec600126b1f40c55a5
MacDeth <jam.dangelo@gmail.com>
2016-09-21 04:26:15 -0400

    Changed language 2 to most likely support final version of tokenizer

--- inclass/prog1/Language2.java
+++ inclass/prog1/Language2.java
@@ -13,21 +13,34 @@ public class Language2{
   }
 
   private static boolean isExpression(){
-    if(true)
-      return true;
+    if(isDigit()){
+      if(tokenizer.isEndOfString())
+        return true;
+      else if(checkChars(new char[]{'+', '-'}) && isExpression())
+        return true;
+    }
     return false;
   }
 
   private static boolean isDigit(){
-    if(true)
-      return true;
-    return false;
+    return checkChars(
+      new char[]{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}
+    );
   }
 
   private static boolean isID(){
-    if(true)
+    if(checkChars(new char[]{'a', 'b'}))
       return true;
     return false;
   }
 
+  private static boolean checkChars(char[] query){
+    char token = tokenizer.getNextToken();
+    for(int i = 0; i < query.length; i++){
+      if(token == query[i]){
+        return true;
+      }
+    }
+    return false;
+  }
 }
