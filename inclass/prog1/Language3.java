bc49f87da0c2aff3534babba7d8fe1e9fa8146dc
MacDeth <jam.dangelo@gmail.com>
2016-09-21 18:32:20 -0400

    formatting

--- inclass/prog1/Language3.java
+++ inclass/prog1/Language3.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class Language3{
   private static Tokenizer tknzr;
   public Language3(ArrayList<String> lineList) {
-		this.tknzr = new Tokenizer(lineList);
+		tknzr = new Tokenizer(lineList);
 		for (int i = 0; i < lineList.size(); i++) {
 			System.out.println("Line " + i + " was " + check());
 		}
