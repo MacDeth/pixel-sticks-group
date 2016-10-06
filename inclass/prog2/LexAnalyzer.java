fb246979a265964614a71bd0ddd99010a6de9e01
MacDeth <jam.dangelo@gmail.com>
2016-10-06 13:37:27 -0400

    Layout for lex?

--- inclass/prog2/LexAnalyzer.java
+++ inclass/prog2/LexAnalyzer.java
@@ -1,7 +1,36 @@
-
 public class LexAnalyzer{
+  private int position = 0;
+  private char character;
+  private String[] input;
 
-  public String lex(){
-    return "";
+  public void setInput(String input){
+    String[] temp = input.split("\\b *"); // Input preprocessing for boundaries
+    for(int i = 0; i < temp.length; i++){
+      temp[i].trim();
+    }
   }
+
+  public Token lex(){
+
+    return Token.END_OF_INPUT; // Placeholder
+  }
+
+  public void advance(){
+
+  }
+
+  public char curChar(){
+    return character;
+  }
+
+  public int pos(){
+    return position;
+  }
+
+  public char getNext(){
+    char temp = character;
+    advance();
+    return temp;
+  }
+
 }
